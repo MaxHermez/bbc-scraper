c863207707b5fb04e1cecee5f8c0dd78463a8d81
maxhermez <Max.Hermez@gmail.com>
2023-08-04 13:29:09 +0300

    add database connection pipeline stage

--- news_scraper/pipelines.py
+++ news_scraper/pipelines.py
@@ -5,10 +5,27 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
 from itemadapter import ItemAdapter
+from os import getenv
 
 
 class MongoPipeline:
-    collection_name = "articles"
+    collection_name = 'articles'
+
+    def __init__(self):
+        self._mongo_uri = getenv("MONGO_URI", None)
+        self._mongo_db = getenv("MONGO_DB", "Scrapy_db")
+        self._mongo_articles = getenv("MONGO_COL", "Articles")
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self._mongo_uri)
+        self.db = self.client[self._mongo_db]
+        self.articles_col = self.db[self._mongo_articles]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.articles_col.insert_one(dict(item))
         return item
